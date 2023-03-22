ec6ca59304100b4612e6abde6ff61b5f6ff6c9f3
VolodyaZaluskyi <127944525+VolodiaZaluskyi@users.noreply.github.com>
2023-03-22 14:45:57 +0200

    Lab3 Zaluskyi Volodymyr TR-15

---
+++ pairs.py
@@ -0,0 +1,10 @@
+def FindPairSum(number, numbers):
+ for i in numbers:
+    for j in numbers:
+        if (i + j == number) & (i != j):
+            print(f"{i} + {j} = 10")
+            
+arr_num = [1, 2, 3, 4, 5, 6, 7]
+
+FindPairSum(10, arr_num)
+input()
