cb145d6aa125309494e6c5347d6c207c59ac7ba5
Yurij Kysylov <87658578+YuggOgg@users.noreply.github.com>
2023-03-29 10:41:36 +0300

    Update pairs.py

--- Yurii Kysylov/Lab_3/pairs.py
+++ Yurii Kysylov/Lab_3/pairs.py
@@ -1,9 +1,9 @@
-def Print(_i, _j):
-    print(_i, "+", _j)
+def Sum(first, second):
+    if first + second == 10:
+        print(first, "+", second)
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+m = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-for i in numbers:
-    for j in numbers:
-        if i + j == 10:
-            Print(i, j)
+for i in m:
+    for j in m:
+        Sum(i, j)
