9d3f0032ea6f83cf3d52e992286d916ddac3c695
VolodyaZaluskyi <127944525+VolodiaZaluskyi@users.noreply.github.com>
2023-04-18 02:18:30 +0300

    Update py_module.py

--- Labs_Zaluskyi_Volodymyr/lab_4/py_module.py
+++ Labs_Zaluskyi_Volodymyr/lab_4/py_module.py
@@ -1,18 +1,15 @@
-a, countl_z, count_and = False, 0, 0
-
-while a != True:
-    file = open(input('Enter puth to file: '), 'r')
+while True:
+    file = open(input('Введіть шлях до файлу: '), 'r')
     count_lines = file.readlines()
     empty_lines = [line for line in count_lines if line == '\n']
     z_lines = [line for line in count_lines if 'z' in line]
     count_z = len(z_lines)
     count_z_lines = [line for line in count_lines for z in line if z == 'z']
     and_lines = [line for line in count_lines]
-    for line in and_lines:
-        if 'and' in line: count_and += 1
-    print(f'total lines: {len(count_lines)}\nempty lines: {len(empty_lines)}\n')
-    print(f'lines with "z": {count_z}\n')
-    print(f'"z" count": {len(count_z_lines)}\nlines with "and": {count_and}\n')
+    count_and = sum('and' in line for line in and_lines)
+    print(f'Всього рядків: {len(count_lines)}\nпорожніх рядків: {len(empty_lines)}')
+    print(f'Рядків з "z": {count_z}')
+    print(f'Кількість "z": {len(count_z_lines)}\nРядків з "and": {count_and}')
 
-    key = str(input('Exit(x) | Analize new file(f)\nEnter char:'))
-    a = True if key == 'x' else False if key == 'y' else True
+    key = str(input('Вийти (x) | Проаналізувати новий файл (f)\nВведіть символ: '))
+    if key == 'x': break
